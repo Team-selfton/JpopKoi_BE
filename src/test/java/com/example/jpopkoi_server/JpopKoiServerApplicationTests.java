13de7df64e842be5b9759cc3c22f7253fdfab442
박태수 <taesupark0504@gmail.com>
2025-08-09 03:43:50 +0900

    chore :: setting

---
+++ src/test/java/com/example/jpopkoi_server/JpopKoiServerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.jpopkoi_server;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JpopKoiServerApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
